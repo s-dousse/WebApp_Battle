18dd2d7da2f4b409fc75c81f7fb52f8c3a1c9e3f
Sarah <dousse.sarah@gmail.com>
2021-12-02 22:02:29 +0000

    new feature: attacking the other player

---
+++ spec/features/attacking_player2_spec.rb
@@ -0,0 +1,10 @@
+feature "attacking player2" do
+  scenario 'player1 attacks player2 and gets a confirmation' do
+    sign_in_and_play
+
+    save_and_open_page # will save the web page and open the browser to display it
+
+    click_button 'Attack'
+    expect(page).to have_content "Succesful attack : Snoopy looses 20HP"
+  end
+end
