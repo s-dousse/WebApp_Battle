64618614e30ba2873d572829bcd5c753a6444db5
Sarah <dousse.sarah@gmail.com>
2021-12-02 16:01:18 +0000

    views: form gets name of 2 players && new name.erb to display the names

--- app.rb
+++ app.rb
@@ -10,6 +10,13 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @name1 = params[:name1]
+    @name2 = params[:name2]
+    p params
+    erb :names
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
