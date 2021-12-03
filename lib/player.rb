7d8e4c4f6814947c4bf2fe9e68cc77affcfaa4d7
Sarah <dousse.sarah@gmail.com>
2021-12-03 12:22:21 +0000

    TDD: player class has an attack method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,18 @@
 class Player
   attr_reader :name, :hp
   DEFAULT_HP = 60
+  RECEIVED_HIT = 10
 
   def initialize(name)
     @name = name
     @hp = DEFAULT_HP
+  end
+
+  def attack(other_player)
+   other_player.loose_hp
+  end
+
+  def loose_hp
+    @hp -= RECEIVED_HIT
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,30 @@
 require 'player'
 
 describe Player do
-    let!(:player1) { Player.new("Sally") }
 
-    it "returns its name" do
+  context "Player set up" do
+      let!(:player1) { Player.new("Sally") }
+
+      it "returns its name" do
         expect(player1.name).to eq "Sally"
-    end
+      end
 
-    it "starts with a max number of HP" do
+      it "has a default number of HP when initialised" do
         expect(player1.hp).to eq 60
-    end
+      end
+  end
+
+  context "Player's actions" do
+      let!(:player1) { Player.new("Sally") }
+      let!(:player2) { Player.new("Linus") }
+
+      it "can attack another player" do
+        expect(player1).to respond_to(:attack).with(1).argument
+      end
+
+      it "can reduce the hp of other player" do
+        player1.attack(player2)
+        expect(player2.hp).to eq 50
+      end
+   end
 end
